6c6f0357065709190bc20e7ecd89fdadaacdf557
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:27 -0500

    xml commenting

--- src/Soenneker.Notion.HttpClients/Abstract/INotionOpenApiHttpClient.cs
+++ src/Soenneker.Notion.HttpClients/Abstract/INotionOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Notion.HttpClients.Abstract;
 /// </summary>
 public interface INotionOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
